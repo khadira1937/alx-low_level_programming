c8608445e64d9cb7401d21db79e36e967b858b57
oussamakhadira <okhadira@gmail.com>
2023-03-16 18:02:03 -0700

    0x00. C - Hello, World

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,3 @@
-#!/bin/bash
 #include<stdio.h>
 /**
  * main - Entry point
@@ -10,4 +9,11 @@ int main(void)
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
+
+
+5-printf.c
+
+#include<stdio.h>
+/**
+* main - Entry point
 
